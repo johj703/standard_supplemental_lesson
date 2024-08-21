2c43a26a4c5237e6a9d01d3ee81646d30875ddd6
Jo <johj703@nate.com>
2024-08-21 14:42:08 +0900

    안보고 다시 작성

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,9 @@ const App = () => {
 
         {/* TDOO: Route를 이용해서 about, like, mypage 페이지 만들기 */}
         {/* 별도 컴포넌트 파일을 만들지 말고 App.jsx 안에서 바로 작성하기 */}
+        <Route path="/about" element={<div>about 페이지 입니다.</div>} />
+        <Route path="/like" element={<div>like 페이지 입니다.</div>} />
+        <Route path="/mypage" element={<div>mypage 페이지 입니다.</div>} />
       </Routes>
     </BrowserRouter>
   );
