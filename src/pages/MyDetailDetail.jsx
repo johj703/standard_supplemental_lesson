3ca3935d26c3a9529e77c6a720daa9d7d9414b68
Jo <johj703@nate.com>
2024-08-21 15:14:41 +0900

    MyDetailDetailPage를 MyDetailDetail로 변경

--- src/pages/MyDetailDetail.jsx
+++ src/pages/MyDetailDetail.jsx
@@ -1,18 +1,21 @@
-import React from "react";
 import { useParams, useNavigate } from "react-router-dom";
 
 const MyDetailDetailPage = () => {
+  const navigate = useNavigate();
   const params = useParams();
   const name = params.name;
   const age = params.age;
 
-  const GoHome = () => {
-    useNavigate("/Home");
-  };
   return (
     <div>
       {age}살이신 {name}님 안녕하세요!
-      <button onClick={GoHome}>홈으로</button>
+      <button
+        onClick={function () {
+          navigate("/");
+        }}
+      >
+        홈으로
+      </button>
     </div>
   );
 };
