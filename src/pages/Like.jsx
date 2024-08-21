22f7bd9a2382171e3036c22894c669f314f21edd
Jo <johj703@nate.com>
2024-08-21 14:49:34 +0900

    About, Home, Like, MyPage 페이지 만들기

---
+++ src/pages/Like.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Like = () => {
+  return <div>Like 페이지 입니다.</div>;
+};
+
+export default Like;
---
+++ src/pages/MyPage.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MyPage = () => {
+  return <div>MyPage 페이지 입니다.</div>;
+};
+
+export default MyPage;
