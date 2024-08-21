b38f437b12f3d23f7840ef2069d278a63145410a
Jo <johj703@nate.com>
2024-08-21 15:29:58 +0900

    40살로 변경 버튼을 누르면 setSearchParams에 의해 모두 비워져버린다.

--- src/pages/MyPage.jsx
+++ src/pages/MyPage.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { useSearchParams } from "react-router-dom";
 
 const MyPage = () => {
-  return <div>MyPage 페이지 입니다.</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const name = searchParams.get("name");
+  const age = searchParams.get("age");
+
+  console.log(name);
+  console.log(age);
+  return (
+    <div>
+      {name}님 안녕하세요. {age}살 이시군요.
+      <button
+        style={{
+          backgroundColor: "red",
+          color: "white",
+        }}
+        onClick={function () {
+          setSearchParams();
+        }}
+      >
+        40살로 변경
+      </button>
+    </div>
+  );
 };
 
 export default MyPage;
