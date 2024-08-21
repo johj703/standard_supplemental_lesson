8122ac19b7acd144dd990bdec9f8a019b58e6083
Jo <johj703@nate.com>
2024-08-21 14:49:16 +0900

    각 페이지를 import하고 return 값에 넣기

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./pages/About";
+import Like from "./pages/Like";
+import MyPage from "./pages/MyPage";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
@@ -10,9 +14,10 @@ const App = () => {
 
         {/* TDOO: Route를 이용해서 about, like, mypage 페이지 만들기 */}
         {/* 별도 컴포넌트 파일을 만들지 말고 App.jsx 안에서 바로 작성하기 */}
-        <Route path="/about" element={<div>about 페이지 입니다.</div>} />
-        <Route path="/like" element={<div>like 페이지 입니다.</div>} />
-        <Route path="/mypage" element={<div>mypage 페이지 입니다.</div>} />
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Like" element={<Like />} />
+        <Route path="/MyPage" element={<MyPage />} />
       </Routes>
     </BrowserRouter>
   );
