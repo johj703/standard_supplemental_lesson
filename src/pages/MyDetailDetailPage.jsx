e89992c10aeab316e3fd0c0acae0f018542bc0ec
Jo <johj703@nate.com>
2024-08-21 15:06:20 +0900

    MyDetailDetailPage에서 age와 name을 동적으로 나타내도록 작성

--- src/pages/MyDetailDetailPage.jsx
+++ src/pages/MyDetailDetailPage.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { useParams, useNavigate } from "react-router-dom";
 
 const MyDetailDetailPage = () => {
-  return <div>N살이신 ~~님 안녕하세요!</div>;
+  const params = useParams();
+  const name = params.name;
+  const age = params.age;
+
+  const GoHome = () => {
+    useNavigate("/Home");
+  };
+  return (
+    <div>
+      {age}살이신 {name}님 안녕하세요!
+      <button onClick={GoHome}>홈으로</button>
+    </div>
+  );
 };
 
 export default MyDetailDetailPage;
