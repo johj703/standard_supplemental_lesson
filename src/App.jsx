79c6e52815cf5acac1666df4573cbb592e3c2b26
Jo <johj703@nate.com>
2024-08-21 14:55:45 +0900

    app 페이지 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 import About from "./pages/About";
 import Like from "./pages/Like";
 import MyPage from "./pages/MyPage";
-import Home from "./pages/Home";
+import MyPageDetail from "./pages/MyPageDetail";
 
 const App = () => {
   return (
@@ -15,9 +16,10 @@ const App = () => {
         {/* TDOO: Route를 이용해서 about, like, mypage 페이지 만들기 */}
         {/* 별도 컴포넌트 파일을 만들지 말고 App.jsx 안에서 바로 작성하기 */}
         <Route path="/" element={<Home />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Like" element={<Like />} />
-        <Route path="/MyPage" element={<MyPage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/like" element={<Like />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/mypage/:name" element={<MyPageDetail />} />
       </Routes>
     </BrowserRouter>
   );
